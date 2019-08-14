0b4243af3472894b4fdc8d20714a418502466863
Satyam Soni <42893127+SatyamSoni23@users.noreply.github.com>
2019-08-14 23:20:05 +0530

    shop detail and owner detail frontend

--- app/src/main/java/com/example/e_receipt/MainActivity.java
+++ app/src/main/java/com/example/e_receipt/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.e_receipt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import jp.wasabeef.blurry.Blurry;
 
 public class MainActivity extends AppCompatActivity {
-
+    RelativeLayout rootView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rootView = findViewById(R.id.rootView);
+
     }
 }
