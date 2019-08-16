c4d39acf294534cd0d636e3c5cdb8750daa69a10
Satyam Soni <42893127+SatyamSoni23@users.noreply.github.com>
2019-08-16 17:03:58 +0530

    Access denied frontend

---
+++ app/src/main/java/com/example/e_receipt/shopLogoUpload.java
@@ -0,0 +1,13 @@
+package com.example.e_receipt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class shopLogoUpload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_logo_upload);
+    }
+}
