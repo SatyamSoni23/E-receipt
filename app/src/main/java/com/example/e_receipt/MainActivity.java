1b3ddc7692918b6f0f00c8a81c948b8616defcf2
Satyam Soni <42893127+SatyamSoni23@users.noreply.github.com>
2019-08-17 16:54:17 +0530

    bottom_navigation_bar

--- app/src/main/java/com/example/e_receipt/MainActivity.java
+++ app/src/main/java/com/example/e_receipt/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.e_receipt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import jp.wasabeef.blurry.Blurry;
 
 public class MainActivity extends AppCompatActivity {
     RelativeLayout rootView;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rootView = findViewById(R.id.rootView);
-
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startHomeAcitvity();
+            }
+        });
+    }
+    public void startHomeAcitvity(){
+        Intent intent = new Intent(this, home.class);
+        startActivity(intent);
     }
 }
