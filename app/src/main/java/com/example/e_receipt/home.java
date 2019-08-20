fbbe6908068c98802149d92ac3c3523861f8ce6f
Satyam Soni <42893127+SatyamSoni23@users.noreply.github.com>
2019-08-20 21:49:41 +0530

    navigation_bar

--- app/src/main/java/com/example/e_receipt/home.java
+++ app/src/main/java/com/example/e_receipt/home.java
@@ -1,13 +1,63 @@
 package com.example.e_receipt;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class home extends AppCompatActivity {
-
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        dl = (DrawerLayout) findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
+
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.aboutEreceipt){
+                    Toast.makeText(home.this, "About E-Receipt", Toast.LENGTH_SHORT).show();
+
+                }
+                else if(id == R.id.updatePassword){
+                    Toast.makeText(home.this, "Update Password", Toast.LENGTH_SHORT).show();
+
+                }
+                else if(id == R.id.contactUs){
+                    Toast.makeText(home.this, "Contact Us", Toast.LENGTH_SHORT).show();
+
+                }
+                else if(id == R.id.Logout){
+                    Toast.makeText(home.this, "Logout", Toast.LENGTH_SHORT).show();
+
+                }
+                return false;
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
