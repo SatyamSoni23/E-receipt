d6b2da1e56e7100117075f46bbae5a5341492981
Satyam Soni <42893127+SatyamSoni23@users.noreply.github.com>
2019-08-21 23:26:53 +0530

    register page

--- app/src/main/java/com/example/e_receipt/registerPage.java
+++ app/src/main/java/com/example/e_receipt/registerPage.java
@@ -1,12 +1,26 @@
 package com.example.e_receipt;
 
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class registerPage extends AppCompatActivity {
+    DatabaseReference rootRef, demoRef, demoRef1;
+    EditText uname, pwd, rePwd;
+    public static String username, password, rePassword;
     Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,16 +28,92 @@ public class registerPage extends AppCompatActivity {
         setContentView(R.layout.activity_register_page);
 
         next = findViewById(R.id.next);
+        uname = findViewById(R.id.username);
+        pwd = findViewById(R.id.password);
+        rePwd = findViewById(R.id.rePassword);
 
+        uname.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    uname.setHint("");
+                    uname.setTypeface(Typeface.create("serif-monospace", Typeface.BOLD_ITALIC));
+                }
+                else {
+                    uname.setHint("Username");
+                }
+            }
+        });
+
+        pwd.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    pwd.setHint("");
+                    pwd.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    pwd.setTypeface(Typeface.create("serif-monospace", Typeface.BOLD_ITALIC));
+                }
+                else{
+                    pwd.setHint("Password");
+                }
+            }
+        });
+
+        rePwd.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    rePwd.setHint("");
+                    rePwd.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    rePwd.setTypeface(Typeface.create("serif-monospace", Typeface.BOLD_ITALIC));
+                }
+                else{
+                    rePwd.setHint("Re-password");
+                }
+            }
+        });
+
+        rootRef = FirebaseDatabase.getInstance().getReference();
+        demoRef = rootRef.child("E-Receipt");
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivityNext();
+                username = uname.getText().toString();
+                password = pwd.getText().toString();
+                rePassword = rePwd.getText().toString();
+                demoRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.child(username).exists()){
+                            Toast.makeText(registerPage.this, "username already registered",Toast.LENGTH_LONG).show();
+                        }
+                        else
+                        {
+                            if(password.equals(rePassword)){
+                                demoRef1 = demoRef.child(username);
+                                demoRef1.child("username").setValue(username);
+                                demoRef1.child("password").setValue(password);
+                                startActivityNext();
+                            }
+                            else{
+                                Toast.makeText(registerPage.this, "Password does not match",Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        startOfflineActivity();
+                    }
+                });
             }
         });
     }
     public void startActivityNext(){
         Intent intent = new Intent(this, shopFirstDetail.class);
             startActivity(intent);
+    }
+    public void startOfflineActivity(){
+        Intent intent = new Intent(this, offline.class);
+        startActivity(intent);
     }
 }
