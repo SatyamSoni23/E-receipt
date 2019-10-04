acecba825bf73642c76c6d7ff65f5bbe604d913d
Satyam Soni <42893127+SatyamSoni23@users.noreply.github.com>
2019-10-04 23:12:41 +0530

    Email Verification

--- app/src/main/java/com/example/e_receipt/shopFirstDetail.java
+++ app/src/main/java/com/example/e_receipt/shopFirstDetail.java
@@ -3,6 +3,7 @@ package com.example.e_receipt;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,14 +11,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class shopFirstDetail extends AppCompatActivity {
     DatabaseReference rootRef, demoRef, demoRef1;
+    private FirebaseAuth mAuth;
     Button next;
     EditText shopName, shopMobile, shopAddress, shopPincode, shopEmail, gstNumber, slogan;
     public static String strShopName, strShopMobile, strShopAddress, strShopPincode, strShopEmail, strGstNumber, strSlogan;
+    ProgressDialog nDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,12 +40,13 @@ public class shopFirstDetail extends AppCompatActivity {
         gstNumber = findViewById(R.id.gstNumber);
         slogan = findViewById(R.id.slogan);
 
+        mAuth = FirebaseAuth.getInstance();
         rootRef = FirebaseDatabase.getInstance().getReference();
         demoRef = rootRef.child("E-Receipt").child(registerPage.username);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ProgressDialog nDialog;
+
                 nDialog = new ProgressDialog(shopFirstDetail.this);
                 nDialog.setMessage("Loading..");
                 nDialog.setIndeterminate(false);
@@ -116,17 +125,44 @@ public class shopFirstDetail extends AppCompatActivity {
                 else{
                     demoRef1.child("slogan").setValue(strSlogan);
                 }
-
-                startNextActivity();
+                mAuth.createUserWithEmailAndPassword(strShopEmail, registerPage.password)
+                        .addOnCompleteListener(shopFirstDetail.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    sendVerificationEmail();
+                                } else {
+                                    startOfflineActivity();
+                                }
+                            }
+                        });
             }
         });
     }
-    public void startNextActivity(){
-        Intent intent = new Intent(this, shopLogoUpload.class);
+    public void startOfflineActivity(){
+        Intent intent = new Intent(this, offline.class);
         startActivity(intent);
     }
     @Override
     public void onBackPressed() {
         Toast.makeText(shopFirstDetail.this, "Please fill the remaining details", Toast.LENGTH_SHORT).show();
+    }
+    private void sendVerificationEmail(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(shopFirstDetail.this, shopLogoUpload.class));
+                    nDialog.dismiss();
+                    finish();
+                }
+                else{
+                    Toast.makeText(shopFirstDetail.this, "signup Failed",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
